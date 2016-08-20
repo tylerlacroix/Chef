e7428903aca8bc37186daba0ace927ef2a48c918
tylerlacroix <tlacroi2@uwo.ca>
2016-08-20 11:40:32 -0400

    Original Push

---
+++ ChiefHelper/CameraViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChiefHelper
+//
+//  Created by Tyler Lacroix on 8/20/16.
+//  Copyright © 2016 Tyler Lacroix. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ChiefHelper/RecipeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeViewController.swift
+//  ChiefHelper
+//
+//  Created by Tyler Lacroix on 8/20/16.
+//  Copyright © 2016 Tyler Lacroix. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+}
