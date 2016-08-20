bcad31760bb4909383856ca717c6f886fa84c93a
Project-79 <richard.lacroix@icloud.com>
2016-08-20 15:59:36 -0400

    Added Half Working Labels
    They kind of work

--- ChiefHelper/CameraViewController.swift
+++ ChiefHelper/CameraViewController.swift
@@ -16,8 +16,10 @@ class CameraViewController: UIViewController {
     
     @IBOutlet weak var overlayCamera: UIView!
     
+    
     var captureDevice : AVCaptureDevice?
     var previewLayer : AVCaptureVideoPreviewLayer?
+    var labels = [UILabel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,6 +46,10 @@ class CameraViewController: UIViewController {
                 }
             }
         }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -83,7 +89,30 @@ class CameraViewController: UIViewController {
             stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
                 (imageDataSampleBuffer, error) -> Void in
                 let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-                UIImageWriteToSavedPhotosAlbum(UIImage(data: imageData)!, nil, nil, nil)
+                //UIImageWriteToSavedPhotosAlbum(UIImage(data: imageData)!, nil, nil, nil)
+                var foodLabel = UILabel(frame: CGRectMake(250, 500, 100, 40))
+                foodLabel.textAlignment = NSTextAlignment.Right
+                foodLabel.backgroundColor = UIColor.orangeColor()
+                foodLabel.text = "Orange"
+                foodLabel.font = UIFont(name: "Montserrat-Light", size: 16.0)
+                foodLabel.textColor = UIColor.whiteColor()
+                foodLabel.sizeToFit()
+                foodLabel.layer.borderColor = UIColor.orangeColor().CGColor
+                foodLabel.layer.borderWidth = 3.0;
+                foodLabel.layer.cornerRadius = 2
+                foodLabel.layer.masksToBounds = true
+                
+                
+                self.labels.append(foodLabel)
+                
+                self.view.addSubview(foodLabel)
+                
+                UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseOut, animations: {
+                    var foodLabelFrame = self.labels[self.labels.endIndex - 1].frame
+                    foodLabelFrame.origin.y -= CGFloat(350 - ((self.labels.endIndex - 1)*50))
+                    
+                    self.labels[self.labels.endIndex - 1].frame = foodLabelFrame
+                    }, completion: {_ in})
             }
         }
     }
