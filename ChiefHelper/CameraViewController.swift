db5d97c5c5596989aede3b72e17623d7c61b5fa1
Project-79 <richard.lacroix@icloud.com>
2016-08-20 14:47:10 -0400

    Added full screen camera
    It’s pretty  :)

--- ChiefHelper/CameraViewController.swift
+++ ChiefHelper/CameraViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    let stillImageOutput = AVCaptureStillImageOutput()
+    
+    @IBOutlet weak var overlayCamera: UIView!
+    
+    var captureDevice : AVCaptureDevice?
+    var previewLayer : AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+        if captureSession.canAddOutput(stillImageOutput) {
+            captureSession.addOutput(stillImageOutput)
+        }
+    
+        let devices = AVCaptureDevice.devices()
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        print("Capture device found")
+                        beginSession()
+                    }
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    
+    func beginSession() {
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+            // use input
+        } catch {
+            fatalError("Could not create capture device input.")
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        let bounds:CGRect = view.layer.bounds;
+        previewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        previewLayer!.bounds = bounds;
+        previewLayer!.position=CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds));
+        
+        let newview = UIView(frame: self.view.frame)
+        self.view.insertSubview(newview, atIndex: 0)
+        
+        newview.layer.addSublayer(previewLayer!)
+        previewLayer?.frame = overlayCamera.bounds
+        captureSession.startRunning()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
+            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
+                (imageDataSampleBuffer, error) -> Void in
+                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+                UIImageWriteToSavedPhotosAlbum(UIImage(data: imageData)!, nil, nil, nil)
+            }
+        }
+    }
 
 }
 
